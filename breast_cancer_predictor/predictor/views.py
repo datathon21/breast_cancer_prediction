9eb28c003048adefccc0bd1b21e6da2e467f444b
Hai <lhhai.1208@gmail.com>
2021-10-16 13:34:02 -0500

    add plots

--- breast_cancer_predictor/predictor/views.py
+++ breast_cancer_predictor/predictor/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def predict(request):
+    return render(request, 'predictForm.html')
